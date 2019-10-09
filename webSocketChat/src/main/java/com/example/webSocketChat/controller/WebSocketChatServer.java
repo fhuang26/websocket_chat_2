474ed15bfb0f705fd87eec01345c3b5cf118b705
Felix Huang <fhuang726@gmail.com>
2019-10-09 15:17:43 -0700

    Add files via upload

---
+++ webSocketChat/src/main/java/com/example/webSocketChat/controller/WebSocketChatServer.java
@@ -0,0 +1,100 @@
+package com.example.webSocketChat.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.example.webSocketChat.model.Message;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * WebSocket Server
+ *
+ * @see ServerEndpoint WebSocket Client
+ * @see Session   WebSocket Session
+ */
+
+@Component
+@ServerEndpoint("/chat")
+public class WebSocketChatServer {
+
+    /**
+     * All chat sessions.
+     */
+    private static Map<String, Session> onlineSessions = new ConcurrentHashMap();
+    private static Map<String, String> sessionUser = new ConcurrentHashMap();
+
+    private static void sendMessageToAll(String msg) {
+        //TODO: add send message method.
+        onlineSessions.forEach((id, session) -> {
+            try {
+                session.getBasicRemote().sendText(msg);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        /*
+        for (Map.Entry<String, Session> e: onlineSessions.entrySet()) {
+            Session session = e.getValue();
+            session.getAsyncRemote().sendText(msg);
+        }
+        */
+    }
+
+    /**
+     * Open connection, 1) add session, 2) add user.
+     */
+    @OnOpen
+    public void onOpen(Session session) throws IOException {
+        //TODO: add on open connection.
+        onlineSessions.put(session.getId(), session);
+        // sendMessageToAll(Message.jsonStr("", "", Message.ENTER, onlineSessions.size()));
+
+        System.out.println("len of hash map=" + onlineSessions.size() + "  session=" + session.getId());
+    }
+
+    /**
+     * Send message, 1) get username and session, 2) send message to all.
+     */
+    @OnMessage
+    public void onMessage(Session session, String jsonStr) throws IOException {
+        //TODO: add send message.
+        // sendMessageToAll(jsonStr);
+        System.out.println("@@@ enter onMessage()");
+
+        Message message = JSON.parseObject(jsonStr, Message.class);
+        System.out.println("@@@" + message.getUsername() + " " + message.getMessage() + " " + message.getType());
+        sessionUser.put(session.getId(), message.getUsername());
+        sendMessageToAll(Message.jsonStr(message.getUsername(), message.getMessage(), message.getType(),
+                "" + onlineSessions.size()));
+    }
+
+    /**
+     * Close connection, 1) remove session, 2) update user.
+     */
+    @OnClose
+    public void onClose(Session session) throws IOException {
+        //TODO: add close connection.
+        System.out.println("session=" + session.getId() + "  size of hash map=" + onlineSessions.size() +
+                "  size of sessionUser=" + sessionUser.size());
+        onlineSessions.remove(session.getId());
+        if (sessionUser.containsKey(session.getId())) {
+            String user = sessionUser.get(session.getId());
+            sendMessageToAll(Message.jsonStr(user, "offline", Message.QUIT, "" + onlineSessions.size()));
+            sessionUser.remove(session.getId());
+        }
+        session.close();
+    }
+
+    /**
+     * Print exception.
+     */
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+
+}
